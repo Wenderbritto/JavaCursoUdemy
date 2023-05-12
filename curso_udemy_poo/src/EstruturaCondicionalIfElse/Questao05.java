75cf81fb77ba4aba56a93775450886bcddd8eb0d
wenderbritto <wenderbrito.dev@gmail.com>
2023-05-12 16:52:37 -0300

    Resolução

--- curso_udemy_poo/src/EstruturaCondicionalIfElse/Questao05.java
+++ curso_udemy_poo/src/EstruturaCondicionalIfElse/Questao05.java
@@ -1,6 +1,8 @@
 package EstruturaCondicionalIfElse;
 /*Com base na tabela abaixo, escreva um programa que leia o código de um item e a quantidade deste item. A 
 seguir, calcule e mostre o valor da conta a pagar*/
+
+
 import java.util.Scanner;
 public class Questao05 {
 
