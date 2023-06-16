dde14813a8a3cece1e34711cd03baca3cdf42eec
Wenderbritto <wenderbrito.dev@gmail.com>
2023-06-15 22:10:19 -0300

    explicacao do codigo

--- curso_udemy_poo/src/ClasseAtributoMetodo/Program.java
+++ curso_udemy_poo/src/ClasseAtributoMetodo/Program.java
@@ -1,24 +1,43 @@
 package ClasseAtributoMetodo;
 import java.util.Scanner;
+import java.util.Locale;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
 		Scanner leitor = new Scanner(System.in);
 		
+		//Instanciando os objetos do tipo Triangle.
 		Triangle x = new Triangle();
 		Triangle y = new Triangle();
 		
+		//Atribuindo valores para os atributos do objeto x.
 		System.out.println("Enter the measures of triangle X: ");
 		x.a= leitor.nextDouble();
 		x.b= leitor.nextDouble();
 		x.c= leitor.nextDouble();
 		
+		//Atribuindo valores para os atributos do objeto y.
 		System.out.println("Enter the measures of triangle Y: ");
 		y.a= leitor.nextDouble();
 		y.b= leitor.nextDouble();
 		y.c= leitor.nextDouble();
+		
+		//Declarando variaveis com o metodo para calcular a area.
+		double areaX= x.area();
+		double areaY= y.area();
+		
+		System.out.printf("Triangle x area: %.4f%n",areaX);
+		System.out.printf("Triangle y area: %.4f%n",areaY);
+		
+		if(areaX > areaY) {
+			System.out.println("Larger area: X");
+		}
+		else {
+			System.out.println("Larger area: Y");
+		}
 		
 
 	}
--- curso_udemy_poo/src/ClasseAtributoMetodo/Triangle.java
+++ curso_udemy_poo/src/ClasseAtributoMetodo/Triangle.java
@@ -1,11 +1,14 @@
 package ClasseAtributoMetodo;
 
+//Criando uma classe publica.
 public class Triangle {
 	
+	//Declarando os atributos dessa classe.
 	public double a;
 	public double b;
 	public double c;
 	
+	//Criando um metodo para calcular a area.
 	public double area() {
 		
 		double p =(a+b+c)/2.0;
