a022ebc26431cb0dccd6cc1bb11ec5d13a71640b
Wenderbritto <wenderbrito.dev@gmail.com>
2023-06-16 22:19:36 -0300

    Exercicio fixacao

---
+++ curso_udemy_poo/src/ClasseAtributoMetodo3/Program.java
@@ -0,0 +1,15 @@
+package ClasseAtributoMetodo3;
+/*Fazer um programa para ler os dados de um funcionario
+(nome, salario bruto e imposto). Em seguida, mostrar os 
+dados do funcionario (nome e salario liquido). Em seguida,
+aumentar o salario do funcionario com base em uma porcentagem
+dada (somente o salario bruto e afetado pela porcentagem) e mostrar
+novamente os dados do funcionario.*/
+public class Program {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
