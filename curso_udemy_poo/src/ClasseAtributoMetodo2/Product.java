2ec45062c24de3f22b2f9d8d341514669af9abb7
Wenderbritto <wenderbrito.dev@gmail.com>
2023-06-16 21:01:45 -0300

    exercicio de POO aula2

---
+++ curso_udemy_poo/src/ClasseAtributoMetodo2/Product.java
@@ -0,0 +1,29 @@
+package ClasseAtributoMetodo2;
+
+public class Product {
+	
+	public String name;
+	public Double price;
+	public int quantity;
+	
+	
+	public double totalValueInStock() {
+		
+		return price * quantity;
+		
+	}
+	
+	public void addProducts(int quantity) {
+		
+		this.quantity += quantity;
+		
+	}
+	
+	public void removeProducts(int quantity) {
+		
+		this.quantity -= quantity;
+		
+		
+	}
+
+}
---
+++ curso_udemy_poo/src/ClasseAtributoMetodo2/Program.java
@@ -0,0 +1,23 @@
+package ClasseAtributoMetodo2;
+
+import java.util.Locale;
+import java.util.Scanner;
+/*Fazer um programa para ler os dados de um produto em estoque 
+(nome, preco e quantidade no estoque).Em seguida: 
+* Mostrar os dados do produto (nome, preco, quantidade no estoque, 
+valor total do estoque);
+* Realizar uma entrada no estoque e mostrar novamente os dados do produto;
+* Realizar uma saida no estoque e mostrar novamente os dados do produto.*/
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner leitor = new Scanner(System.in);
+		
+		Product product = new Product();
+		
+
+	}
+
+}
