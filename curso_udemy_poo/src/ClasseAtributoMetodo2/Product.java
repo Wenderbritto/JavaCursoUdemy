469d7bb90567b936ddd37803822e624b15375fba
Wenderbritto <wenderbrito.dev@gmail.com>
2023-06-16 21:57:15 -0300

    explicacao do codigo

--- curso_udemy_poo/src/ClasseAtributoMetodo2/Product.java
+++ curso_udemy_poo/src/ClasseAtributoMetodo2/Product.java
@@ -1,30 +1,35 @@
 package ClasseAtributoMetodo2;
 
+//Classe produto
 public class Product {
 	
+	//Atributos da classe
 	public String name;
 	public Double price;
 	public int quantity;
 	
-	
+	//Metodo para calcular o valor do estoque
 	public double totalValueInStock() {
 		
 		return price * quantity;
 		
 	}
 	
+	//Metodo para adicionar produtos no estoque.
 	public void addProducts(int quantity) {
 		
 		this.quantity += quantity;
 		
 	}
 	
+	//Metodo para remover produtos do estoque.
 	public void removeProducts(int quantity) {
 		
 		this.quantity -= quantity;
 		
 	}
 	
+	//Metodo para imprimir os dados usando o toString.
 	public String toString() {
 		
 		return name 
--- curso_udemy_poo/src/ClasseAtributoMetodo2/Program.java
+++ curso_udemy_poo/src/ClasseAtributoMetodo2/Program.java
@@ -15,35 +15,46 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner leitor = new Scanner(System.in);
 		
+		//Instanciando um objeto do tipo Product
 		Product product = new Product();
 		
 		System.out.println("Enter product data: ");
 		
+		//Vai inserir um nome no atributo do objeto product.
 		System.out.print("Name: ");
 		product.name = leitor.nextLine();
 		
+		//Vai inserir o preco no atributo do objeto product.
 		System.out.print("Price: ");
 		product.price = leitor.nextDouble();
 		
+		//Vai inserir uma quantidade de estoque no atributo do objeto produt.
 		System.out.print("Quantity in stock: ");
 		product.quantity = leitor.nextInt();
 		
+		//Vai imprimir as informacoes do objeto product, conforme foi formatado no metodo toString.
 		System.out.println();
 		System.out.println("Product data: " + product);
 		
+		//Vai solicitar que o usuario digite
 		System.out.println();
 		System.out.println("Enter the number of product to be added in stock: ");
 		int quantity = leitor.nextInt();
+		//E o metodo addProducts vai receber o valor digitado pelo usuario.
 		product.addProducts(quantity);
 		
+		//Vai imprimir as informacoes do objeto product, conforme foi formatado no metodo toString.
 		System.out.println();
 		System.out.println("Updated data: " + product);
 		
+		//Vai solicitar que o usuario digite
 		System.out.println();
 		System.out.println("Enter the number of product to be removed from stock: ");
 		quantity = leitor.nextInt();
+		//E metodo removeProducts vai receber o valor digitado pelo usuario.
 		product.removeProducts(quantity);
 		
+		//Vai imprimir as informacoes do objeto product, conforme foi formatado no metodo toString.
 		System.out.println();
 		System.out.println("Updated data: " + product);
 	}
