718335a3e18e7919eb2425bdd2a4b2a5e2e48820
Wenderbritto <wenderbrito.dev@gmail.com>
2023-07-05 22:22:01 -0300

    continuacao da resolucao

--- curso_udemy_poo/src/ExercicioSecao9/Conta.java
+++ curso_udemy_poo/src/ExercicioSecao9/Conta.java
@@ -6,10 +6,22 @@ public class Conta {
 	private String nome;
 	private double saldo;
 	
+	public Conta(int numero, String nome, double saldo) {
+		this.numero = numero;
+		this.nome = nome;
+		this.saldo = saldo;
+	}
+	
 	public Conta(int numero, String nome) {
 		this.numero = numero;
 		this.nome = nome;
 	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
 	public void deposito(double saldo) {
 		this.saldo += saldo;
 	}
@@ -17,5 +29,10 @@ public class Conta {
 		this.saldo -= saldo -5;
 	}
 	
+	public String toString() {
+		return "Nome: " + nome +
+				", numero: " + numero +
+				", saldo: " + saldo;
+	}
 
 }
--- curso_udemy_poo/src/ExercicioSecao9/Main.java
+++ curso_udemy_poo/src/ExercicioSecao9/Main.java
@@ -1,10 +1,31 @@
 package ExercicioSecao9;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
+		Scanner leitor = new Scanner(System.in);
 		
+		System.out.println("Digite o numero da conta: ");
+		int numConta = leitor.nextInt();
+		
+		System.out.println("Digite seu nome: ");
+		String nome = leitor.next();
+		
+		System.out.println("Vai fazer o deposito inicial?(S/N)");
+		String init = leitor.next();
+		
+		if(init == "S") {
+			
+			
+		}
+		
+		Conta c1 = new Conta(numConta, nome);
+		System.out.println();
+		
+		System.out.println("Dados da Conta: "+c1);
 
 	}
 
